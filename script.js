b5970d9059ed552c49f585322c0b45c82baa5129
Ashish1208-create <at605075@gmail.com>
2026-05-25 22:50:34 +0530

    removed nested OneDrive folder

--- script.js
+++ script.js
@@ -1,41 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
 
 const app = express();
-// const port = 3000;
 const port = process.env.PORT || 3000;
-
-// const db = new pg.Client({
-//   user: "postgres",
-//   host: "localhost",
-//   database: "flags",
-//   password: "june",
-//   port: 5432,
-// });
-// db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 let items = [
   { id: 1, title: "Buy milk" },
   { id: 2, title: "Finish homework" },
 ];
-
-// app.get("/", async (req, res) => {
-//   try {
-//     const result = await db.query("SELECT * FROM items ORDER BY id ASC");
-//     items = result.rows;
-
-//     res.render("index.ejs", {
-//       listTitle: "Today",
-//       listItems: items,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
 
 app.get("/", (req, res) => {
   res.render("index.ejs", {
@@ -43,17 +19,6 @@ app.get("/", (req, res) => {
     listItems: items,
   });
 });
-
-// app.post("/add", async (req, res) => {
-//   const item = req.body.newItem;
-//   // items.push({title: item});
-//   try {
-//     await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
-//     res.redirect("/");
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
 
 app.post("/add", (req, res) => {
   const item = req.body.newItem;
@@ -66,39 +31,18 @@ app.post("/add", (req, res) => {
   res.redirect("/");
 });
 
-// app.post("/edit", async (req, res) => {
-//   const item = req.body.updatedItemTitle;
-//   const id = req.body.updatedItemId;
-
-//   try {
-//     await db.query("UPDATE items SET title = ($1) WHERE id = $2", [item, id]);
-//     res.redirect("/");
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
-app.post("/edit", async (req, res) => {
+app.post("/edit", (req, res) => {
   const item = req.body.updatedItemTitle;
-  const id = req.body.updatedItemId;
+  const id = parseInt(req.body.updatedItemId);
 
-  try {
-    await db.query("UPDATE items SET title = ($1) WHERE id = $2", [item, id]);
-    res.redirect("/");
-  } catch (err) {
-    console.log(err);
+  const foundItem = items.find((i) => i.id === id);
+
+  if (foundItem) {
+    foundItem.title = item;
   }
-});
 
-// app.post("/delete", async (req, res) => {
-//   const id = req.body.deleteItemId;
-//   try {
-//     await db.query("DELETE FROM items WHERE id = $1", [id]);
-//     res.redirect("/");
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
+  res.redirect("/");
+});
 
 app.post("/delete", (req, res) => {
   const id = parseInt(req.body.deleteItemId);
