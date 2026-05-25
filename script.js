0124ee036ae6eeccfb72a8003edffd9abd9f62c3
Ashish1208-create <at605075@gmail.com>
2026-05-25 23:58:20 +0530

    fixed static path

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
