9fe8a031e224ec302671e04902033fe8124c2f52
Ashish1208-create <at605075@gmail.com>
2026-05-25 23:09:36 +0530

    fixed views path

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 let items = [
   { id: 1, title: "Buy milk" },
