cbe472296e0d453ee7bc66b73223074d1bb926e1
Ashish1208-create <at605075@gmail.com>
2026-05-25 22:05:13 +0530

    removed local db for vercel

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000;
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "flags",
-  password: "june",
-  port: 5432,
-});
-db.connect();
+// const db = new pg.Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "flags",
+//   password: "june",
+//   port: 5432,
+// });
+// db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -22,29 +23,47 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", async (req, res) => {
-  try {
-    const result = await db.query("SELECT * FROM items ORDER BY id ASC");
-    items = result.rows;
+// app.get("/", async (req, res) => {
+//   try {
+//     const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+//     items = result.rows;
 
-    res.render("index.ejs", {
-      listTitle: "Today",
-      listItems: items,
-    });
-  } catch (err) {
-    console.log(err);
-  }
+//     res.render("index.ejs", {
+//       listTitle: "Today",
+//       listItems: items,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    listTitle: "Today",
+    listItems: items,
+  });
 });
 
-app.post("/add", async (req, res) => {
+// app.post("/add", async (req, res) => {
+//   const item = req.body.newItem;
+//   // items.push({title: item});
+//   try {
+//     await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
+//     res.redirect("/");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+app.post("/add", (req, res) => {
   const item = req.body.newItem;
-  // items.push({title: item});
-  try {
-    await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
-    res.redirect("/");
-  } catch (err) {
-    console.log(err);
-  }
+
+  items.push({
+    id: items.length + 1,
+    title: item,
+  });
+
+  res.redirect("/");
 });
 
 app.post("/edit", async (req, res) => {
